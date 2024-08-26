1080baf497b876e61194452971b8b41c10837c25
Roberto Arce <robertoarcee@gmail.com>
2024-08-26 01:12:22 -0400

    implements features

--- src/calculator.py
+++ src/calculator.py
@@ -1,23 +1,26 @@
+import math
+
+
 class Calculator:
     def __init__(self):
         pass
 
     def factorial(self, n):
         """Calcula el factorial de un número."""
-        raise NotImplementedError("Este método debe ser implementado.")
+        return 1 if n == 0 else n * self.factorial(n - 1)
 
     def fibonacci(self, n):
         """Calcula el n-ésimo número de la secuencia de Fibonacci."""
-        raise NotImplementedError("Este método debe ser implementado.")
+        return n if n <= 1 else self.fibonacci(n - 1) + self.fibonacci(n - 2)
 
     def power(self, base, exponent):
         """Calcula la potencia de un número elevado a otro."""
-        raise NotImplementedError("Este método debe ser implementado.")
+        return base**exponent
 
     def exponential(self, value):
         """Calcula la función exponencial e^x."""
-        raise NotImplementedError("Este método debe ser implementado.")
+        return math.exp(value)
 
     def hyperbolic_sine(self, value):
         """Calcula el seno hiperbólico de un valor."""
-        raise NotImplementedError("Este método debe ser implementado.")
+        return math.sinh(value)
